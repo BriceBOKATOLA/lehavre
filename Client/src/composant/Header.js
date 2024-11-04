9a8632cd86420eefc7b966630ad8bcd87a1aadca
Romain Bailhache <romain.bailhache@gmail.com>
2024-11-04 11:18:21 +0100

    ajout fichier, ajout gitinore

---
+++ Client/src/composant/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../image/LeHavreSeine.png'
+
+export default function Header() {
+  return (
+    <div>
+        <img src={logo} alt="logo Le Havre Seine Métropole"/>
+        <h1>CALENDRIER DES ÉVÈNEMENTS</h1>
+    </div>
+  )
+}
