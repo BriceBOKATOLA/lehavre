190e4f6c9f7f8b3eb1fd906469bd8a976682bb15
Romain Bailhache <romain.bailhache@gmail.com>
2024-11-04 13:45:17 +0100

    header menu fini avec FontAwesome ajouté

--- Client/src/composant/Header.js
+++ Client/src/composant/Header.js
@@ -1,11 +1,28 @@
-import React from 'react'
-import logo from '../image/LeHavreSeine.png'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import logo from '../image/LeHavreSeine.png';
+import burger from '../image/menu.png';
+import '../style/header.css';
+
 
 export default function Header() {
   return (
-    <div>
-        <img src={logo} alt="logo Le Havre Seine Métropole"/>
-        <h1>CALENDRIER DES ÉVÈNEMENTS</h1>
+    <div class="header">
+        <div class="header-logo">
+            <img src={logo} alt="logo Le Havre Seine Métropole"/>
+        </div>
+        <div class="header-text">
+            <p>CALENDRIER DES ÉVÈNEMENTS</p>
+        </div>
+        <div class="header-menu">
+            <div>
+                <FontAwesomeIcon icon={faBars} size='2x' />
+            </div>
+            <div>
+                <p>MENU</p>
+            </div>
+        </div>
     </div>
   )
 }
