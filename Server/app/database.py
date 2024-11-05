457a882e6c48ac23995c351bcbae2369ccb2cd9f
Maeva Noel <44382065+UnicornOfTheNight@users.noreply.github.com>
2024-11-05 11:30:34 +0100

    Update code fonctionnel

--- Server/app/database.py
+++ Server/app/database.py
@@ -3,9 +3,9 @@ import sqlite3
 from sqlite3 import Error
 
 class DataBase:
-    def sql_connection(self):
+    def sql_connection(db_path):
         try:
-            db = sqlite3.connect(self.db_path)
+            db = sqlite3.connect(db_path)
             return db
         except Error:
             print(Error)
@@ -22,56 +22,72 @@ class DataBase:
             cur = con.cursor()
             cur.execute(query, data)
             con.commit()
-            print("Utilisateur ajouté.")
-        except Error:
-            print(Error)
+            print(f"Utilisateur {data[0]} ajouté")
+        except Error as e:
+            print(f"Erreur lors de l'ajout de l'utilisateur: {e}")
 
-    def DeleteUser(con, data):
-        query = "DELETE FROM user WHERE username = ?;"
-        try:
-            cur = con.cursor()
-            cur.execute(query, (data,))
-            con.commit()
-            print("Utilisateur supprimé.")
-        except Error:
-            print(Error)
-
-    def ModifyUser(con, data, id):
-        query = "UPDATE user SET username = ?, pwd = ?  WHERE id = ?"
-        try:
-            cur = con.cursor()
-            cur.execute(query, (data, id))
-            con.commit()
-            print("Utilisateur modifié.")
-        except Error:
-            print(Error)
-
-    def CreateEvenement(con, data):
-        query = "INSERT INTO event (title, date_begin, date_end, place, event_type, organisators, description) VALUES(?,?,?,?,?,?,?)"
-        try:
-            cur = con.cursor()
-            cur.execute(query, data)
-            con.commit()
-            print("Evenement ajouté.")
-        except Error:
-            print(Error)
-
-    def DeleteEvenement(con, id):
-        query = "DELETE FROM event WHERE id = ?;"
+    def DeleteUser(con, id):
+        query = "DELETE FROM user WHERE id = ? RETURNING username"
         try:
             cur = con.cursor()
             cur.execute(query, (id,))
-            con.commit()
-            print("Evenement supprimé.")
-        except Error:
-            print(Error)
+            result = cur.fetchone()
 
-    def ModifyEvenement(con, data, id):
+            if result:
+                con.commit()
+                print(f"Utilisateur {result[0]} supprimé.")
+        except Error as e:
+            print(f"Erreur lors de la suppression de l'utilisateur: {e}")
+
+    def UpdateUser(con, data, id):
+        query = "UPDATE user SET username = ?, pwd = ?  WHERE id = ?"
+        try:
+            cur = con.cursor()
+            cur.execute(query, (*data, id))
+            con.commit()
+            print(f"Utilisateur {data[0]} modifié.")
+        except Error as e:
+            print(f"Erreur lors de la modification de l'utilisateur: {e}")
+
+    def CreateEvenement(con, data):
+        query = ("INSERT OR IGNORE INTO event (title, date_begin, date_end, place, event_type, organisators, description) "
+                 "VALUES(?,?,?,?,?,?,?)")
+        try:
+            cur = con.cursor()
+             # Check if the event was actually inserted
+            cur.execute("SELECT EXISTS(SELECT 1 FROM event WHERE title = ? AND date_begin = ? AND place = ?)", 
+                        (data[0], data[1], data[3]))
+            exists = cur.fetchone()[0]  # fetchone returns a tuple
+            if exists:
+                print(f"L'évenement {data[0]} existe déja.")
+
+            else:
+                cur.execute(query, data)
+                con.commit()
+                print(f"Evenement {data[0]} a été ajouté.")
+                
+        except Error as e:
+            print(f"Erreur lors de l'ajout de l'évenement: {e}")
+
+    def DeleteEvenement(con, id):
+        query = "DELETE FROM event WHERE id = ? RETURNING title"
+        try:
+            cur = con.cursor()
+            cur.execute(query, (id,))
+            result = cur.fetchone()
+
+            if result:
+                con.commit()
+                print(f"Evenement {result[0]} supprimé.")
+        except Error as e:
+            print(f"Erreur lors de la suppression de l'évenement: {e}")
+
+    def UpdateEvenement(con, data, id):
         query = "UPDATE event SET title = ?, date_begin = ? ,date_end = ? ,place = ? ,event_type = ? ,organisators = ? ,description = ?  WHERE id = ?"
         try:
             cur = con.cursor()
-            cur.execute(query, (data, id))
+            cur.execute(query, (*data, id))
             con.commit()
-            print("Evenement modifié.")
-        except Error:
-            print(Error)
+            print(f"Evenement {data[0]} modifié.")
+        except Error as e:
+            print(f"Erreur lors de la modification de l'évenement: {e}")
--- Server/app/main.py
+++ Server/app/main.py
@@ -1,18 +1,24 @@
 from database import DataBase
 from dataExtractor import DataExtractor
+import sys
+import os
 
+# Ajouter le chemin vers le dossier racine du projet
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from Client.src.db.init_db import InitDb  # Import de la classe InitDb
 
 class Main:
     def __init__(self):
-        self.file_path_json = '.\\Server\\Data\\data.json'
-        self.db_path = '..\\Client\\src\\db\\init_db.py'
-        self.con = None  # Connexion à la base de données
+        self.file_path_json = '..\\Data\\data.json'
+        self.db_path = '..\\..\\Client\\src\\db\\LHSM_events.db'
+        #self.db_initializer = InitDb
+        self.con = DataBase.sql_connection(self.db_path)  # Connexion à la base de données
         self.data_extractor = DataExtractor(self.file_path_json, self.db_path)
 
     def run(self):
+        #self.db_initializer.init_db()
+        InitDb.init_db()
         # Connexion à la base de données
-        DataBase.sql_connection(self)
-        #print(self.data_extractor.load_json())
         # Charger les données JSON
         data = self.data_extractor.load_json()
         # Exécuter les méthodes CRUD sur les utilisateurs
@@ -21,6 +27,9 @@ class Main:
             # Créer un utilisateur
             DataBase.CreateUser(self.con, (user['username'], user['pwd']))
 
+        #DataBase.ModifyUser(self.con, ("janedoe2", "securepass4562"), 3)
+        
+        #DataBase.DeleteUser(self.con, 4)
         # Exécuter les méthodes CRUD sur les événements
         for event in data['events']:
             # Créer un événement
@@ -35,7 +44,7 @@ class Main:
             ))
 
         # Fermer la connexion
-        self.sql_close()
+        DataBase.sql_close(self.con)
 
 
 if __name__ == "__main__":
